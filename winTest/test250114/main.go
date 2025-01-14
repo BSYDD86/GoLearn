92cc3f9feaa41bc71de107f59d3c33bd078f5595
BSYDD86 <z861744988@gmail.com>
2025-01-14 13:59:58 +0800

    win250114 start learn karl

--- winTest/test250114/main.go
+++ winTest/test250114/main.go
@@ -1,1 +1,33 @@
-package test250114
+package main
+
+func main() {
+
+}
+func lemonadeChange(bills []int) bool {
+	five := 0
+	ten := 0
+	for bill := range bills {
+		if bill == 5 {
+			five++
+		} else if bill == 10 {
+			if five < 0 {
+				return false
+			} else {
+				five--
+				ten++
+			}
+		} else if bill == 20 {
+			if ten >= 2 {
+				ten -= 2
+			} else if ten >= 1 && five >= 1 {
+				ten--
+				five--
+			} else if five >= 3 {
+				five -= 3
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
